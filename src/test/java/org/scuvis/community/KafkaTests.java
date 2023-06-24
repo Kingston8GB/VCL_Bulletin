af1ab6e6bb3a4334ad10863efb9f8999b91d37fa
lxy <xiyao_li710@126.com>
2023-06-25 06:23:21 +0800

    20230625-0623

---
+++ src/test/java/org/scuvis/community/KafkaTests.java
@@ -0,0 +1,55 @@
+package org.scuvis.community;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author Xiyao Li
+ * @date 2023/06/18 02:20
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = CommunityApplication.class)
+public class KafkaTests {
+
+    @Autowired
+    Producer producer;
+
+    @Autowired
+    Comsumer consumer;
+
+    @Test
+    public void test(){
+        producer.send("test","111");
+        producer.send("test","222");
+
+    }
+}
+@Component
+class Producer{
+    @Autowired
+    KafkaTemplate kafkaTemplate;
+
+    public void send(String topic, String data){
+        kafkaTemplate.send(topic,data);
+    }
+}
+@Component
+class Comsumer{
+    @Autowired
+    KafkaTemplate kafkaTemplate;
+
+    @KafkaListener(topics = {"test"})
+    public void handle(){
+
+    }
+}
